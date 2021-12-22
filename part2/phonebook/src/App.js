fdb8e7fc05aacacf84f858cdf314c220941bba34
Andrés Pereyra <andrespereyra.it@gmail.com>
2021-12-21 21:52:45 -0300

    2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [searchedNames, setSearchedNames] = useState([]);
   const [newSearch, setNewSearch] = useState("");
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [matchedName, setMatchedName] = useState(false);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      const { data } = response;
+      setPersons(data);
+    });
+  }, []);
 
   const filterSearch = (input) => {
     const searchResult = persons.filter((person) => {
