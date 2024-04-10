2b1f985a695594f977bd65c412986b44ddb8d320
andrespereyra-it <andrespereyra.it@gmail.com>
2024-04-10 15:08:04 -0300

    search countries by name

---
+++ part2/countries/src/components/Finder.js
@@ -0,0 +1,14 @@
+export const Finder = ({ inputCountry, setInputCountry, setShowCountry }) => {
+  const handleChange = (e) => {
+    const inputValue = e.target.value;
+    setInputCountry(inputValue);
+    setShowCountry(false);
+  };
+
+  return (
+    <form>
+      <label>find countries</label>
+      <input value={inputCountry} onChange={handleChange}></input>
+    </form>
+  );
+};
